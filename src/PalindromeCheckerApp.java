2f2b6d8190c624e2effe006587158ab79fa5f333
hydra-vignesh <vigneshprakash2510@gmail.com>
2026-03-10 21:13:20 +0530

    UC9: Recursive Palindrome Checker

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Input : ");
+        String input = sc.nextLine();
+
+        boolean result = check(input, 0, input.length() - 1);
+
+        System.out.println("Is Palindrome? : " + result);
+
+        sc.close();
+    }
+
+    private static boolean check(String s, int start, int end) {
+        if (start >= end) {
+            return true;
+        }
+
+        if (s.charAt(start) != s.charAt(end)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
+    }
+}
--- src/uc1.java
+++
@@ -1,9 +0,0 @@
-public class uc1 {
-
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Version: 1.0");
-        System.out.println("Application started successfully.");
-    }
-}
